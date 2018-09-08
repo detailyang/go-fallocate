219dd93241f34438fa670d79abe9e1a19d420501
detailyang <detailyang@gmail.com>
2018-09-08 19:34:01 +0800

    tests: tiny tweaks

--- fallocate_linux_test.go
+++ fallocate_linux_test.go
@@ -3,7 +3,7 @@ package fallocate
 import "testing"
 
 func TestDarwinFallocate(t *testing.T) {
-	sizes := []int64{392, 4, 1, 0, 99999}
+	sizes := []int64{392, 4, 1, 237, 99999}
 	for _, size := range sizes {
 		fallocateWithNewFile(t, size)
 	}
